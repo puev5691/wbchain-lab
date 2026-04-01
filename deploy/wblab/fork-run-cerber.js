baab4de79edf5bc84f7e1b0c1c5ccac24d20401d
puev5691 <pustolenko.evgeny@yandexx.ru>
2026-04-01 17:03:23 +0300

    Add WBLAB fork configs for cerber ridmik finik

---
+++ deploy/wblab/fork-run-cerber.js
@@ -0,0 +1,12 @@
+global.FORK_MODE = 1;
+global.NETWORK = "WBLAB";
+global.START_NETWORK_DATE = 1775052991842;
+global.CONSENSUS_PERIOD_TIME = 1000;
+global.START_PORT_NUMBER = 30000;
+global.HTTP_PORT_NUMBER = 8000;
+
+global.FORK_IP_LIST = [
+  {"ip":"10.241.96.86","port":30000}, // CERBER
+  {"ip":"10.241.96.41","port":30001}, // RIDMIK
+  {"ip":"10.241.96.200","port":30002} // FINIK
+];
