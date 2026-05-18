566617404c68ed815c17318f93d42438bd4464f3
ЛуЦзенХяо <pustolenko.evgeny@yandex.ru>
2026-05-18 18:21:45 +0300

    Add WBN shard identity config

---
+++ deploy/wbn-node/configs/shard.js
@@ -0,0 +1,13 @@
+global.NETWORK = "WELLBEING";
+
+global.SHARD_NAME = "WBN";
+
+global.START_NETWORK_DATE = 1778186522932;
+
+global.SeedServerArr = [
+    {ip:"185.39.19.240", port:30000, Score:10000000, System:1},
+];
+
+global.CONSENSUS_PERIOD_TIME = 3000;
+global.START_HISTORY = 1;
+global.START_MINING = 10;
